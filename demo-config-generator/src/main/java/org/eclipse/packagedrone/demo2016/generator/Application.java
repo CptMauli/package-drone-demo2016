cce9441282d4887cd3c09170a5fcea64b4ab8abb
Juergen Rose <juergen.rose@ibh-systems.com>
2016-06-17 12:44:54 +0200

    initial project structure

---
+++ demo-config-generator/src/main/java/org/eclipse/packagedrone/demo2016/generator/Application.java
@@ -0,0 +1,9 @@
+package org.eclipse.packagedrone.demo2016.generator;
+
+public class Application {
+
+	public static void main(String[] args) {
+		new Generator().run();
+	}
+
+}
---
+++ demo-config-generator/src/main/java/org/eclipse/packagedrone/demo2016/generator/Generator.java
@@ -0,0 +1,10 @@
+package org.eclipse.packagedrone.demo2016.generator;
+
+public class Generator {
+
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
