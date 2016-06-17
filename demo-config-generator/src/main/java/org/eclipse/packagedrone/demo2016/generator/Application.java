1d564aaa6a987ab4457cb75c53c205c723d09351
Juergen Rose <juergen.rose@ibh-systems.com>
2016-06-17 14:18:39 +0200

    added example to create debian & rpm packages test content change Revert "test content change"
    This reverts commit a4e40adebc3ac5ab7aaec23e8b69becb271c6aa2.

--- demo-config-generator/src/main/java/org/eclipse/packagedrone/demo2016/generator/Application.java
+++ demo-config-generator/src/main/java/org/eclipse/packagedrone/demo2016/generator/Application.java
@@ -2,8 +2,12 @@ package org.eclipse.packagedrone.demo2016.generator;
 
 public class Application {
 
-	public static void main(String[] args) {
-		new Generator().run();
-	}
+    public static void main(String[] args) {
+        try {
+            new Generator().run();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
 }
